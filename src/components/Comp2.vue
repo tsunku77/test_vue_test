2d80c6790abd7b88dc2a5db6af97a5b81770d3b4
musichour86 <blue_akira@naver.com>
2021-03-12 14:22:13 +0900

    수정7

--- src/components/Comp2.vue
+++ src/components/Comp2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Comp2">
-    <input type="text" v-model="counter" placeholder="할 일을 작성해주세요" @keyup.enter="add_btn" autofocus>
+    <input type="text" v-model="counter" placeholder="할 일을 작성해주세요" @keypress.enter="add_btn" ref="ref_focus" autofocus>
     <button type="button" v-text="btns" @click="add_btn"></button>
   </div>
 </template>
@@ -18,9 +18,11 @@ export default {
       if(this.counter.trim() !== ''){
           this.$store.commit('add_btn',this.counter);
           this.counter = '';
-
+          this.$refs.ref_focus.focus();
       }else {
         alert("할 일을 작성해주세요");
+        this.counter = '';
+        this.$refs.ref_focus.focus();
       }
     },
   }
